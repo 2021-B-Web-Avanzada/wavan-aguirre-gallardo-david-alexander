0fab3bf0cf68632565534d5dffc893c95a8fa17b
David AGUIRRE <david.aguirre@epn.edu.ec>
2021-11-18 09:09:00 -0500

    Acercaimento a aregglos y for

--- 01-javascript/2_arreglos.js
+++ 01-javascript/2_arreglos.js
@@ -1,1 +1,27 @@
 //ARREGLOS
+let arreglo = [6,7,8,9,10];
+arreglo = true;
+arreglo = undefined;
+arreglo = null;
+arreglo = {};
+arreglo = [true, 1, 1.1, "David","Alexander", undefined, null, {},[1,2]];
+arreglo = [6,7,8,9,10];
+
+
+
+
+//Tipos de For
+//For of y for in
+//for of
+for (let number of arreglo) {
+//valores
+    console.log('numero',number);
+}
+for (let indice in arreglo) {//INDICES
+    arreglo[indice];
+    console.log('indice',indice);
+}
+let objetoPrueba = {a:'1',b:'2',c:'3'};
+for (let llave in objetoPrueba) {
+    console.log('llave',llave);
+}
