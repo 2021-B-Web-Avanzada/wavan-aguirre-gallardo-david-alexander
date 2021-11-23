2a5ed525f1582d677d59d12df881d50fe6843bc6
David Aguirre <david.aguirre@epn.edu.ec>
2021-11-23 09:07:58 -0500

    update/operadores
    se aprende a utilizar operadores en arreglos

--- 01-javascript/2_arreglos.js
+++ 01-javascript/2_arreglos.js
@@ -25,3 +25,22 @@ let objetoPrueba = {a:'1',b:'2',c:'3'};
 for (let llave in objetoPrueba) {
     console.log('llave',llave);
 }
+
+arreglo.push(11);//Aniadir elementos al final
+//[6,7,8,9,10,11]
+arreglo.pop();//eliminal al final un elemento
+//[6,7,8,9,10]
+arreglo.unshift(5);//Aniadir al principio del arreglo
+//[5,6,7,8,9,10]
+console.log(arreglo);
+
+//Metpdp splice agregar en el indice 0
+arreglo.splice(0,0,4);
+//splice(indice,elementos eliminados,itemsagregar)
+//El arreglo.splice(0,3,1,2,3,4,5,6)
+//[4,5,6,7,8,9,10]
+console.log(arreglo);
+const indiceNueve = arreglo.indexOf(9);//ecnuentra el primero elemento y devuelve el indice
+arreglo.splice(indiceNueve,2);
+//[4,5,6,7,8]
+console.log(arreglo);
