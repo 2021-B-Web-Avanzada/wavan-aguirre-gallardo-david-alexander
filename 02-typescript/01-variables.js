e1b81bb9de49a8836b02428547e479e9cdb738f2
David Aguirre <david.aguirre@epn.edu.ec>
2021-12-07 08:50:45 -0500

    update/interfaces
    Se crea interfaces de typescript

---
+++ 02-typescript/01-variables.js
@@ -0,0 +1,24 @@
+//01-variables.ts
+var nombre = 'David'; // primitivo
+var nombre2 = 'David2'; //class string
+//nombre = 1
+nombre = 'Alexander';
+var edad = 32;
+var casado = false;
+var fecha = new Date();
+var sueldo;
+sueldo = 12.4;
+//Duck Typing
+var apellido = 'Eguez'; // string ->
+apellido = 'Aguirre'; //igual a otros string
+apellido.toUpperCase(); //metodos string
+var marihuana = 2;
+marihuana = '2';
+marihuana = true;
+marihuana = function () { return '2'; };
+var edadMultiple = '2';
+edadMultiple = '2';
+edadMultiple = 2222;
+edadMultiple = 'dos';
+edadMultiple = new Date();
+//edadMultiple = true;
