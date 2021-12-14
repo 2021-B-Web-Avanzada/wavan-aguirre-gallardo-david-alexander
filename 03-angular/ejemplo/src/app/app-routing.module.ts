73443e09f8e5245f395f7a32fc27cfcb6a9f33b4
David Aguirre <david.aguirre@epn.edu.ec>
2021-12-14 09:05:50 -0500

    update/routing login, inicio, app, usuario y post

--- 03-angular/ejemplo/src/app/app-routing.module.ts
+++ 03-angular/ejemplo/src/app/app-routing.module.ts
@@ -3,6 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import {RutaLoginComponent} from "./rutas/ruta-login/ruta-login.component";
 import {RutaNotFoundComponent} from "./rutas/ruta-not-found/ruta-not-found.component";
 import {RutaForbiddenComponent} from "./rutas/ruta-forbidden/ruta-forbidden.component";
+import {RutaInicioComponent} from "./rutas/ruta-inicio/ruta-inicio.component";
+import {RutaAppComponent} from "./rutas/ruta-app/ruta-app.component";
+import {RutaPostComponent} from "./rutas/ruta-post/ruta-post.component";
+import {RutaUsuarioComponent} from "./rutas/ruta-usuario/ruta-usuario.component";
+
+
+//Login
+//Inicio
+//App
+  //usuario
+  //user
+//configuracion
+
 
 const routes: Routes = [
   {
@@ -16,6 +29,33 @@ const routes: Routes = [
   {
     path: 'forbidden',
     component: RutaForbiddenComponent,
+  },
+  {
+    path:'inicio',
+    component:RutaInicioComponent
+  },
+  {
+    path:'app',
+    component:RutaAppComponent,
+    children:[
+      {
+        path:'post',
+        component:RutaPostComponent
+      },
+      {
+        path:'usuario',
+        component:RutaUsuarioComponent
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch:'full',
+  },
+  {
+    path: '**',
+    component:RutaNotFoundComponent
   }
 ];
 
--- 03-angular/ejemplo/src/app/app.module.ts
+++ 03-angular/ejemplo/src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { RutaLoginComponent } from './rutas/ruta-login/ruta-login.component';
 import { RutaNotFoundComponent } from './rutas/ruta-not-found/ruta-not-found.component';
 import { RutaForbiddenComponent } from './rutas/ruta-forbidden/ruta-forbidden.component';
+import { RutaInicioComponent } from './rutas/ruta-inicio/ruta-inicio.component';
+import { RutaUsuarioComponent } from './rutas/ruta-usuario/ruta-usuario.component';
+import { RutaPostComponent } from './rutas/ruta-post/ruta-post.component';
+import { RutaAppComponent } from './rutas/ruta-app/ruta-app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RutaLoginComponent,
     RutaNotFoundComponent,
-    RutaForbiddenComponent
+    RutaForbiddenComponent,
+    RutaInicioComponent,
+    RutaUsuarioComponent,
+    RutaPostComponent,
+    RutaAppComponent
   ],
   imports: [
     BrowserModule,
