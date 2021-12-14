115ce44de5b7886989cc4b74abe3d4130fc6024c
David Aguirre <david.aguirre@epn.edu.ec>
2021-12-14 07:33:06 -0500

    update/not-found and forbidden route

--- 03-angular/ejemplo/src/app/app-routing.module.ts
+++ 03-angular/ejemplo/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {RutaLoginComponent} from "./rutas/ruta-login/ruta-login.component";
+import {RutaNotFoundComponent} from "./rutas/ruta-not-found/ruta-not-found.component";
+import {RutaForbiddenComponent} from "./rutas/ruta-forbidden/ruta-forbidden.component";
 
 const routes: Routes = [
   {
     path: 'login',
     component: RutaLoginComponent,
+  },
+  {
+    path: 'not-found',
+    component: RutaNotFoundComponent,
+  },
+  {
+    path: 'forbidden',
+    component: RutaForbiddenComponent,
   }
 ];
 
--- 03-angular/ejemplo/src/app/app.module.ts
+++ 03-angular/ejemplo/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RutaLoginComponent } from './rutas/ruta-login/ruta-login.component';
+import { RutaNotFoundComponent } from './rutas/ruta-not-found/ruta-not-found.component';
+import { RutaForbiddenComponent } from './rutas/ruta-forbidden/ruta-forbidden.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RutaLoginComponent
+    RutaLoginComponent,
+    RutaNotFoundComponent,
+    RutaForbiddenComponent
   ],
   imports: [
     BrowserModule,
