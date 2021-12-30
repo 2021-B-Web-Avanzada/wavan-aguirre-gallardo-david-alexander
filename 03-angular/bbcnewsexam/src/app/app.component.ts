f9bcf0eaebe0327ce1e445d599f26eef51513841
David Aguirre <david.aguirre@epn.edu.ec>
2021-12-30 15:54:24 -0500

    update/angular examen bbc news banner

---
+++ 03-angular/bbcnewsexam/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'bbcnewsexam';
+  navItems = ["Noticias","América Latina","Internacional","Medioambiente","Cornovirus","Hay Festival","Economía","Ciencia","Salud","Cultura","Tecnología","Video"];
+}
