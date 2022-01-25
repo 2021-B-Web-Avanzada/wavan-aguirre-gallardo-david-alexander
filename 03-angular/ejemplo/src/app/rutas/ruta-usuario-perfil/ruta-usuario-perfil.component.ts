71ee6d5d843348a54e8771485c9194be2ba9ab9b
David Aguirre <david.aguirre@epn.edu.ec>
2022-01-25 08:48:35 -0500

    update/ejemplo actualizar usuario

--- 03-angular/ejemplo/src/app/rutas/ruta-usuario-perfil/ruta-usuario-perfil.component.ts
+++ 03-angular/ejemplo/src/app/rutas/ruta-usuario-perfil/ruta-usuario-perfil.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {UserJphService} from "../../services/http/user-jph.service";
 import {UserJphInterface} from "../../services/http/interfaces/user-jph.interface";
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
@@ -16,40 +16,14 @@ export class RutaUsuarioPerfilComponent implements OnInit {
   constructor(
     private readonly activatedRoute:ActivatedRoute,
     private readonly userJph:UserJphService,
-    private readonly formBuilder:FormBuilder
+    private readonly formBuilder:FormBuilder,
+    private readonly router:Router
   ) { }
 
   ngOnInit(): void {
-    this.formGroup = this.formBuilder.group(
-      {
-        email: new FormControl(
-          {
-            value:'',
-            disabled:false
-          },
-          [
-            Validators.required,
-            Validators.minLength(3),
-          ]
-        )
-      }
-    );
-    const cambio$ = this.formGroup.valueChanges;
-    cambio$.subscribe(
-      {
-          next:(valor)=>{
-            if(this.formGroup){
-              console.log(valor,this.formGroup);
-              if(this.formGroup?.valid){
-                console.log('YUPI');
-              }else{
-                console.log(':(');
-              }
-            }
 
-          }
-      }
-    );
+
+
     const parametrRuta$=this.activatedRoute.params;
     parametrRuta$.subscribe(
       (parametrosRuta)=>{
@@ -66,6 +40,7 @@ export class RutaUsuarioPerfilComponent implements OnInit {
         {
           next:(data)=>{
             this.usuarioActual=data;
+            this.prepararFormulario();
           },
           error: (error)=>{
             console.error(error);
@@ -73,5 +48,69 @@ export class RutaUsuarioPerfilComponent implements OnInit {
         }
       )
   }
+  prepararFormulario(){
+    this.formGroup = this.formBuilder.group(
+      {
+        email: new FormControl(
+          {
+            value:this.usuarioActual ? this.usuarioActual.email:'',
+            disabled:false
+          },
+          [
+            Validators.required,
+            Validators.minLength(3),
+          ]
+        )
+      }
+    );
+    const cambio$ = this.formGroup.valueChanges;
+    cambio$.subscribe(
+      {
+        next:(valor)=>{
+          if(this.formGroup){
+            console.log(valor,this.formGroup);
+            if(this.formGroup?.valid){
+              console.log('YUPI');
+            }else{
+              console.log(':(');
+            }
+          }
 
+        }
+      }
+    );
+  }
+
+  prepararObjeto(){
+    if(this.formGroup){
+      const email = this.formGroup.get('email');
+      if(email){
+        return {
+          email:email.value,
+        }
+      }
+    }
+    return {
+      email:'',
+    }
+  }
+  actualizarUsuario(){
+    if(this.usuarioActual){
+      const valoresAActualizar=this.prepararObjeto();
+      const actualizar$ = this.userJph
+        .actualizarPorId(
+          this.usuarioActual.id,
+          valoresAActualizar
+        );
+      actualizar$
+        .subscribe({
+          next: (datos)=>{
+            console.log({datos});
+            const url = ['/app','usuario'];
+            this.router.navigate(url);
+          },
+        });
+    }
+
+  }
 }
