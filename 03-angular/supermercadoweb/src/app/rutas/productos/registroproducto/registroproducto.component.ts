565ab82f83d66a2e3194e5b5fc8c8db878b1929d
David Aguirre <david.aguirre@epn.edu.ec>
2022-02-07 17:12:22 -0500

    update/supermercadosweb hasta registrar mercado

--- 03-angular/supermercadoweb/src/app/rutas/productos/registroproducto/registroproducto.component.ts
+++ 03-angular/supermercadoweb/src/app/rutas/productos/registroproducto/registroproducto.component.ts
@@ -3,6 +3,7 @@ import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {ProductoService} from "../../../services/http/producto/producto.service";
 import {SupermercadoService} from "../../../services/http/supermercado/supermercado.service";
 import {Router} from "@angular/router";
+import {SupermercadoInterface} from "../../../services/http/interfaces/supermercado.interface";
 
 @Component({
   selector: 'app-registroproducto',
@@ -16,6 +17,7 @@ export class RegistroproductoComponent implements OnInit {
     "Electrodomesticos","Aseo Personal","Carnes","Embutidos",
     "Postres","Frutas","Ropa"
   ]
+  arrayMercados!:SupermercadoInterface[];
   constructor(
     private readonly formBuilder:FormBuilder,
     private readonly productoService:ProductoService,
@@ -25,6 +27,18 @@ export class RegistroproductoComponent implements OnInit {
 
   ngOnInit(): void {
     this.prepararFormulario();
+    this.mercadoService.buscarTodos('').subscribe(
+      (datos)=>{
+        console.log(datos);
+        this.arrayMercados=datos;
+      },
+      ()=>{
+
+      },
+      ()=>{
+
+      }
+    )
   }
   prepararFormulario(){
     this.formGroup=this.formBuilder.group(
@@ -117,6 +131,15 @@ export class RegistroproductoComponent implements OnInit {
           [
             Validators.required
           ]
+        ),
+        mercadoAsociado:new FormControl(
+          {
+            value:'',
+            disabled:false
+          },
+          [
+            Validators.required
+          ]
         )
       }
     );
@@ -137,7 +160,65 @@ export class RegistroproductoComponent implements OnInit {
       }
     );
   }
+  prepararObjeto(){
+    if(this.formGroup){
+      const nombreProducto = this.formGroup.get('nombreProducto');
+      const precio = this.formGroup.get('precio');
+      const categoria = this.formGroup.get('categoria');
+      const pesogr = this.formGroup.get('pesogr');
+      const caloriasgr = this.formGroup.get('caloriasgr');
+      const proveedor = this.formGroup.get('proveedor');
+      const stock = this.formGroup.get('stock');
+      let disponibilidad =false;
+      if(stock?.value>=0){
+        disponibilidad=true;
+      }
+      const fechaElab = this.formGroup.get('fechaElab');
+      const fechaVenc= this.formGroup.get('fechaVenc');
+      console.log(this.formGroup.get('mercadoAsociado')?.value);
+      const fkMercado= this.arrayMercados[this.formGroup.get('mercadoAsociado')?.value].idMercado;
+      if(nombreProducto&&precio&&categoria&&pesogr&&caloriasgr&&proveedor&&stock&&fechaElab&&
+      fechaVenc&&fkMercado){
+        return {
+          nombreProducto:nombreProducto.value,
+          precio:precio.value,
+          categoria:this.arrayCategorias[categoria.value],
+          pesogr:pesogr.value,
+          caloriasgr:caloriasgr.value,
+          proveedor:0,
+          stock:stock.value,
+          disponibilidad:disponibilidad,
+          fechaElab:fechaElab.value,
+          fechaVenc:fechaVenc.value,
+          fkMercado:fkMercado
+        }
+      }
+    }
+    return {
+      nombreProducto:'',
+      precio:0,
+      categoria:'',
+      pesogr:0,
+      caloriasgr:0,
+      proveedor:0,
+      stock:0,
+      disponibilidad:false,
+      fechaElab:'01-01-2019',
+      fechaVenc:'01-01-2019',
+      fkMercado:1
+    }
+  }
   registrarProducto(){
-
+    const valoresARegistrar = this.prepararObjeto();
+    const registro$ = this.productoService.registrarProducto(valoresARegistrar);
+    registro$.subscribe(
+      {
+        next:(data)=>{
+          console.log({data});
+          const url = ['/productos','producto'];
+          this.router.navigate(url);
+        }
+      }
+    )
   }
 }
