9bb508a249f3e592e8ac5244a2dd8caae7feafe8
David Aguirre <davidaguirreg@gmail.com>
2022-04-28 00:20:35 -0500

    update/repaso hasta promesas async y await

--- 05_mantisRepaso/Javascript/Repaso.js
+++ 05_mantisRepaso/Javascript/Repaso.js
@@ -1,26 +1,53 @@
 const fs = require('fs')
-function escribirArchivo(contenido, path){
+function leerArchivo(path){
+    let contenidoActual=""
+    const promesaLeerArchivo = new Promise(
+        (resolve,reject)=>{
+            fs.readFile(
+                path,
+                "utf-8",
+                (error,contenido)=>{
+                    if(error){
+                        console.log("Error de lectura de archivo.");
+                        reject("Error leyendo archivo")
+                    }else{
+                        contenidoActual=contenido
+                        console.log("Se lee correctamente ", contenidoActual);
+                        resolve(contenidoActual)
+                    }
+                }
+            )
+        }
+    )
+    return promesaLeerArchivo
+}
+async function escribirArchivo(contenido, path){
+    let contenidoActual=""
+    contenidoActual=await leerArchivo(path)
+    console.log("datos=", contenidoActual)
+    let contenidoActualizado = contenidoActual + '\n'+ contenido
     fs.writeFile(
         path,
-        contenido,
+        contenidoActualizado,
         "utf-8",
         (error)=>{
             if(error){
                 console.log("Error al escribir archivo ",error)
             }else{
-                fs.readFile(
-                    "./archivo.txt",
-                    "utf-8",
-                    (error,contenido)=>{
-                        if(error){
-                            console.log("Error al leer archivo ",error)
-                        }else{
-                            console.log("Leer exitoso y resultado = ", contenido)
-                        }
-                    }
-                )
+                console.log("El archivo se escribio correctamente.")
+                // fs.readFile(
+                //     "./archivo.txt",
+                //     "utf-8",
+                //     (error,contenido)=>{
+                //         if(error){
+                //             console.log("Error al leer archivo ",error)
+                //         }else{
+                //             console.log("Leer exitoso y resultado = ", contenido)
+                //         }
+                //     }
+                // )
             }
         }
     )
 }
-escribirArchivo("Hola Escribiendo Nuevo","./archivo.txt")
+escribirArchivo("Todo bien ?","./archivo.txt")
